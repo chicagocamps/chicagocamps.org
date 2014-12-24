e07522a942f1b5d3b95cc192c4d6b44d6e8a3bfe
Shay Howe <shayhowe@gmail.com>
2014-12-23 19:26:46 -0600

    Header setup

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{category}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
