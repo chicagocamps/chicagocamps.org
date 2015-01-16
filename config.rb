41b5dc0453902a17ea937e8ab76af68129988ac3
Shay Howe <shayhowe@gmail.com>
2015-01-15 22:48:10 -0600

    Updating home page

--- config.rb
+++ config.rb
@@ -18,6 +18,7 @@ activate :blog do |blog|
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
+  blog.publish_future_dated = true
 
   # Enable pagination
   blog.paginate = true
