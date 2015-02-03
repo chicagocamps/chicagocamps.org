6cbad04a87264276ae1f073408fbf14aa5b3bd21
Shay Howe <shayhowe@gmail.com>
2015-02-02 20:33:28 -0600

    Splitting into two blogs

--- config.rb
+++ config.rb
@@ -5,22 +5,19 @@
 Time.zone = "America/Chicago"
 
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
+  blog.name = "events"
   blog.permalink = "{category}/{year}/{title}.html"
-  blog.sources = "{category}/{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layouts/update"
-  blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.sources = "events/{year}-{month}-{day}-{title}.html"
+  blog.layout = "layouts/event"
   blog.publish_future_dated = true
+end
 
-  # Enable pagination
+activate :blog do |blog|
+  blog.name = "updates"
+  blog.permalink = "{category}/{year}/{title}.html"
+  blog.sources = "updates/{year}-{month}-{day}-{title}.html"
+  blog.layout = "layouts/update"
+  blog.summary_separator = /(READMORE)/
   blog.paginate = true
   blog.per_page = 5
   blog.page_link = "{num}"
@@ -35,40 +32,6 @@ activate :autoprefixer  do |config|
 end
 
 Sass::Script::Number.precision = 8
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-with_layout :update do
-  page "podcasts/*"
-  page "posts/*"
-end
-
-page "events/*", layout: :event
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
